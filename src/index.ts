602d8fc515b0fcb957d2ef2941d294754752cdfc
mahadi-init <init.mahadi@gmail.com>
2025-07-24 14:30:46 +0600

    config email and password auth and sessions

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,16 @@ api.get('/', (c) => {
 	return c.json({ message: 'Welcome to api' });
 });
 
-api.get('/hello', (c) => {
-	return c.json({ message: 'Welcome to api' });
+app.get('/session', (c) => {
+	const session = c.get('session');
+	const user = c.get('user');
+
+	if (!user) return c.body(null, 401);
+
+	return c.json({
+		session,
+		user,
+	});
 });
 
 export default app;
--- src/lib/better-auth/index.ts
+++ src/lib/better-auth/index.ts
@@ -3,10 +3,11 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { betterAuth } from 'better-auth';
 import { betterAuthOptions } from './options';
+import { account, session, user, verification } from '../../db/schema/better-auth';
 
 export const auth = (env: CloudflareBindings): ReturnType<typeof betterAuth> => {
 	const sql = neon(env.DATABASE_URL);
-	const db = drizzle(sql);
+	const db = drizzle(sql, { schema: { user, account, session, verification } });
 
 	return betterAuth({
 		...betterAuthOptions,
