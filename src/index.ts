f2c83d3d8e74119aa693ee9d214acdf3bb93b4f3
mahadi-init <init.mahadi@gmail.com>
2025-07-24 14:52:56 +0600

    remove unncessary middleware

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,14 @@ import { Variables } from './types/hono-variables';
 
 const app = new Hono<{ Bindings: CloudflareBindings; Variables: Variables }>();
 
-app.use('*', async (c, next) => {
-	const session = await auth(c.env).api.getSession({ headers: c.req.raw.headers });
-
-	if (!session) {
-		c.set('user', null);
-		c.set('session', null);
-		return next();
-	}
-
-	c.set('user', session.user);
-	c.set('session', session.session);
-	return next();
+app.get('/', (c) => {
+	return c.json({ message: 'Welcome to api' });
 });
 
 const api = app.basePath('/api');
 
 api.on(['POST', 'GET'], '/auth/*', (c) => {
 	return auth(c.env).handler(c.req.raw);
-});
-
-api.get('/', (c) => {
-	return c.json({ message: 'Welcome to api' });
 });
 
 export default app;
