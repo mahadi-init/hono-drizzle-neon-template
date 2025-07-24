7f1f0b33791584a0edd0c9e28775de5c516dd1a2
mahadi-init <init.mahadi@gmail.com>
2025-07-24 11:56:17 +0600

    add better auth integration

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,47 @@
-import { db } from './db/connection';
-import { products } from './db/schema';
 import { Hono } from 'hono';
+import { auth } from './lib/better-auth';
 
-const app = new Hono();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.use('*', async (c, next) => {
+	const session = await auth(c.env).api.getSession({ headers: c.req.raw.headers });
+
+	if (!session) {
+		c.set('user', null);
+		c.set('message', 'Hono is hot!!');
+		c.set('session', null);
+		return next();
+	}
+
+	c.set('user', session.user);
+	c.set('session', session.session);
+	return next();
+});
+
+app.on(['GET', 'POST'], '/api/*', (c) => {
+	return auth(c.env).handler(c.req.raw);
+});
+
+// app.get('/', async (c) => {
+// 	try {
+// 		const result = await db(c).select().from(products);
+//
+// 		return c.json({
+// 			result,
+// 		});
+// 	} catch (error) {
+// 		console.log(error);
+// 		return c.json(
+// 			{
+// 				error,
+// 			},
+// 			400,
+// 		);
+// 	}
+// });
 
 app.get('/', async (c) => {
-	try {
-		const result = await db(c).select().from(products);
-
-		return c.json({
-			result,
-		});
-	} catch (error) {
-		console.log(error);
-		return c.json(
-			{
-				error,
-			},
-			400,
-		);
-	}
+	return c.json({ message: 'Welcome to api' });
 });
 
 export default app;
